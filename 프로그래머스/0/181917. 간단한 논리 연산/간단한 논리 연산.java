9b30c39417fbb84df4af36abd83056391c19d49b
ssohyun-Jung <rhkdhl23@naver.com>
2024-08-11 14:35:09 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.03 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+        boolean answer = true;
+        
+        if((x1 == true || x2 == true) && (x3 == true || x4 == true)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
